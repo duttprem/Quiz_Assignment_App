a94ed1f3a91616f95176a3cc56be6b6e30923e27
duttprem <premdutt083@gmail.com>
2023-05-12 21:41:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,63 @@
-import './App.css';
-import Home from './components/Home/Home';
+import axios from "axios";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./Pages/Home/Home";
+import Quiz from "./Pages/Quiz/Quiz";
+import Result from "./Pages/Result/Result";
 
 function App() {
+  const [questions, setQuestions] = useState();
+  const [name, setName] = useState();
+  const [score, setScore] = useState(0);
+
+  const fetchQuestions = async (category = "", difficulty = "") => {
+    const { data } = await axios.get(
+      `https://opentdb.com/api.php?amount=10${
+        category && `&category=${category}`
+      }${difficulty && `&difficulty=${difficulty}`}&type=multiple`
+    );
+
+    setQuestions(data.results);
+  };
+
   return (
-    <>
-    <div className="App">
-      <Home />
-    </div>
-    </>
+    <BrowserRouter>
+      <div className="app" style={{ backgroundImage: 'url("/ques1.png")' }}>
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home
+                name={name}
+                setName={setName}
+                fetchQuestions={fetchQuestions}
+              />
+            }
+          ></Route>
+          <Route
+            path="/quiz"
+            element={
+              <Quiz
+                name={name}
+                questions={questions}
+                score={score}
+                setScore={setScore}
+                setQuestions={setQuestions}
+              />
+            }
+          ></Route>
+          <Route
+            path="/result"
+            element={<Result name={name} score={score} />}
+          ></Route>
+        </Routes>
+      </div>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
